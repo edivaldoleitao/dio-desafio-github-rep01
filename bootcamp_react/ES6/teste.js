7912449ccf3faa973279be252693f1661f00a289
Edivaldo <mandoritos.ea@gmail.com>
2021-08-21 19:52:36 -0300

    arrow functions exemplo

--- bootcamp_react/ES6/teste.js
+++ bootcamp_react/ES6/teste.js
@@ -3,7 +3,7 @@
 var obj = {
     soma :(a, b = 1) =>  a + b  // arrow function, usando um valor default na entrada
 }
-
+ 
 obj['carro_marca'] = 'marca' // criaão de uma proprieda após a criação do objeto
 
 console.log(obj.soma(2))
