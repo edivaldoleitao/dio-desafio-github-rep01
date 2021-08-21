4770ba5d28adf42b7f102546122c7e64bf873d12
Edivaldo <mandoritos.ea@gmail.com>
2021-08-21 19:59:26 -0300

    arrow functions e objetos

--- bootcamp_react/ES6/teste.js
+++
@@ -1,9 +0,0 @@
-
-// objeto obj 
-var obj = {
-    soma :(a, b = 1) =>  a + b  // arrow function, usando um valor default na entrada
-}
- 
-obj['carro_marca'] = 'marca' // criaão de uma proprieda após a criação do objeto
-
-console.log(obj.soma(2))
